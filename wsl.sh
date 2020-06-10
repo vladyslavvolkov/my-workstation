17691b672f666a664e40b1690dc19853947a6874
Vlad Volkov <vlad@hiberbee.com>
2020-06-10 05:33:36 +0300

    Updated commands

--- wsl.sh
+++ wsl.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env sh
 
-
 distribution=${OS:-alpine}
 
 echo "Upgrading system packages"
 case $distribution in
 alpine)
-  apk upgrade
   apk add --no-cache --update ansible
   rm -rf /var/cache/apk/*
   find /usr/lib/python3/site-packages | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
   ;;
+centos | fedora)
+  yum update -y
+  yum install -y ansible
+  find /usr/lib/python3/site-packages | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+  ;;
 ubuntu | debian)
   apt-get update --yes
-  apt-get full-upgrade --yes
   apt-get install --yes \
     python3-setuptools \
     python3-distutils \
@@ -36,20 +38,35 @@ esac
 echo "Setting default values for WSL distribution"
 config=$(cat <<EOF
 [automount]
-enabled = false
-options = "metadata,umask=022,fmask=011"
+enabled = true
+root = /
+options = "metadata,umask=22,fmask=11"
 mountFsTab = false
+
+[network]
+generateHosts = true
+generateResolvConf = true
+
 [interop]
 enabled = false
 appendWindowsPath = false
+
 [user]
 default = "root"
 EOF
 )
 echo "$config" > /etc/wsl.conf
 
-[ -d /etc/ansible ] || mkdir /etc/ansible
+ansibleDirectory=/etc/ansible
+[ -d $ansibleDirectory ] || mkdir $ansibleDirectory
 echo "Downloading Ansible roles"
 wget -O- https://github.com/Hiberbee/ansible/archive/master.tar.gz | tar xvz -C /etc/ansible --strip=1
-cd /etc/ansible || exit
+cd $ansibleDirectory || exit
+
+if [ -f "$ansibleDirectory/requirements.yml" ]; then
+  echo "Installing required roles"
+  ansible-galaxy install -r requirements.yml
+fi
+
+echo "Running Ansible playbook"
 ansible-playbook playbook.yml
